95da178f7099f784482a0a11cb73419d6f7eb977
Felipe Abreu <felipeabreu.rj@gmail.com>
2022-03-25 07:46:03 -0300

    updating bloc logical

--- lib/features/0_presenter/bloc/address_code_bloc.dart
+++ lib/features/0_presenter/bloc/address_code_bloc.dart
@@ -1,8 +1,7 @@
 import 'dart:async';
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:bloc_concurrency/bloc_concurrency.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter_address_code_api/core/blocs/bloc_with_state.dart';
 import 'package:flutter_address_code_api/core/params/address_code_request_params.dart';
 import 'package:flutter_address_code_api/features/1_domain/entities/address.dart';
 import 'package:flutter_address_code_api/features/1_domain/usecases/get_address_usecase.dart';
@@ -10,13 +9,12 @@ import 'package:flutter_address_code_api/features/1_domain/usecases/get_address_
 part 'address_code_event.dart';
 part 'address_code_state.dart';
 
-class AddressCodeBloc
-    extends BlocWithState<AddressCodeEvent, AddressCodeState> {
+class AddressCodeBloc extends Bloc<AddressCodeEvent, AddressCodeState> {
   AddressCodeBloc(this._getAddressUsecase) : super(AddressCodeInitial()) {
     on<AddressCodeSearch>(
       _addressCodeSearch,
       transformer:
-          concurrent(), // concurrent, sequential, droppable, restartable
+          droppable(), // concurrent, sequential, droppable, restartable
     );
   }
 
@@ -26,13 +24,11 @@ class AddressCodeBloc
     AddressCodeEvent event,
     Emitter<AddressCodeState> emit,
   ) async {
-    await runBlocProcess(() async {
-      emit(AddressCodeLoading());
-      final response = await _getAddressUsecase(event.params);
-      response.fold(
-        (failure) => emit(AddressCodeError(failure.errorMessage)),
-        (address) => emit(AddressCodeDone(address)),
-      );
-    });
+    emit(AddressCodeLoading());
+    final response = await _getAddressUsecase(event.params);
+    response.fold(
+      (failure) => emit(AddressCodeError(failure.errorMessage)),
+      (address) => emit(AddressCodeDone(address)),
+    );
   }
 }
