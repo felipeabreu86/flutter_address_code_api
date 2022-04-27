ab33ff32329f5c6d4beeb04759bcd4332f6dd2c7
Felipe Abreu <felipeabreu.rj@gmail.com>
2022-04-27 16:57:15 -0300

    Formating files

--- lib/layers/01-domain/usecases/get_address_usecase.dart
+++ lib/layers/01-domain/usecases/get_address_usecase.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:dartz/dartz.dart';
 import 'package:flutter_address_code_api/layers/00-core/errors/failures.dart';
 import 'package:flutter_address_code_api/layers/00-core/params/address_code_request_params.dart';
--- lib/layers/02-infrastructure/datasources/remote/address_code_datasource.dart
+++ lib/layers/02-infrastructure/datasources/remote/address_code_datasource.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter_address_code_api/layers/00-core/params/address_code_request_params.dart';
 import 'package:flutter_address_code_api/layers/02-infrastructure/models/address_model.dart';
 
